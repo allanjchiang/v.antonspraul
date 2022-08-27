8380453811b1ae21be9aa6a790cee5b0e31c9882
Allan <allanchiang24@gmail.com>
2022-08-27 17:21:53 +1200

    Add for loop function for triangle of #

---
+++ functions/cin-cout.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main() 
+{
+	for (int i = 1; i <= 5; ++i) {
+		cout << "#";
+	}
+
+	cout << "\n";
+
+	for (int i = 1; i <= 4; ++i) {
+		cout << "#";
+	}
+
+	cout << "\n";
+
+	for (int i = 1; i <= 3; ++i) {
+		cout << "#";
+	}
+
+	cout << "\n";
+
+	for (int i = 1; i <= 2; ++i) {
+		cout << "#";
+	}
+
+	cout << "\n";
+	cout << "#";
+}
+
+
