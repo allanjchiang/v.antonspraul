177ad39d3680311bc3adb7778a8a5bf1eac287e5
Allan <allanchiang24@gmail.com>
2022-08-30 17:03:13 +1200

    Add basic function for 3 digits of luhn checksum

---
+++ functions/luhn-checksum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cin;
+using std::cout; 
+
+int luhn_checksum = 123
+
+int main() 
+{
+	for (int digit = 1; digit <= 3; digit += 2) {
+		luhn_checksum % 10 * 2;
+	}
+}
