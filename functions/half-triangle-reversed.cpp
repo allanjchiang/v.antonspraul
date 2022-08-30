1d65ee30295f64e4ea23d9c564fcd9ac6818a408
Allan <allanchiang24@gmail.com>
2022-08-30 16:43:56 +1200

    Add a nested for loop to create a reversed half triangle

--- functions/half-triangle-reversed.cpp
+++ functions/half-triangle-reversed.cpp
@@ -3,3 +3,12 @@
 using std::cin;
 using std::cout;
 
+int main() 
+{
+	for (int row = 5; row >= 1; row--) {
+		for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
+			cout << "#";
+		}
+		cout << "\n";
+	}
+}
