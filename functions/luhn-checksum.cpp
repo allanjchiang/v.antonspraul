ee95a89803aa0428d41da2a11128a015dbc5e0ea
Allan <allanchiang24@gmail.com>
2022-08-31 17:41:22 +1200

    Successfully validated luhn-checksum by adding parentheses

--- functions/luhn-checksum.cpp
+++ functions/luhn-checksum.cpp
@@ -10,14 +10,13 @@ int luhn_checksum = 123;
 
 int main() 
 {
-	
 	int luhn_checksum_third = luhn_checksum % 10 * 2;
 	cout << luhn_checksum_third << endl;
 	int luhn_checksum_second = luhn_checksum / 10 % 10;
 	cout << luhn_checksum_second << endl;
 	int luhn_checksum_first = luhn_checksum / 100 % 10 * 2;
 	cout << luhn_checksum_first << endl;
-	if ((luhn_checksum_third + luhn_checksum_second + luhn_checksum_first % 10) == 0) {
+	if (((luhn_checksum_third + luhn_checksum_second + luhn_checksum_first) % 10) == 0) {
 		cout << "Valid!" << endl;
 	} else {
 		cout << "Invalid!" << endl;
