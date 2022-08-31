8c64b2efcfcfda88ff5b86b6fd302b38b4a1d0f8
Allan <allanchiang24@gmail.com>
2022-08-31 17:38:12 +1200

    Add modulus and if statements with cout

--- functions/luhn-checksum.cpp
+++ functions/luhn-checksum.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
 using std::cin;
 using std::cout; 
 
-int luhn_checksum = 123
+
+int luhn_checksum = 123;
 
 int main() 
 {
-	for (int digit = 1; digit <= 3; digit += 2) {
-		luhn_checksum % 10 * 2;
+	
+	int luhn_checksum_third = luhn_checksum % 10 * 2;
+	cout << luhn_checksum_third << endl;
+	int luhn_checksum_second = luhn_checksum / 10 % 10;
+	cout << luhn_checksum_second << endl;
+	int luhn_checksum_first = luhn_checksum / 100 % 10 * 2;
+	cout << luhn_checksum_first << endl;
+	if ((luhn_checksum_third + luhn_checksum_second + luhn_checksum_first % 10) == 0) {
+		cout << "Valid!" << endl;
+	} else {
+		cout << "Invalid!" << endl;
 	}
+	
 }
