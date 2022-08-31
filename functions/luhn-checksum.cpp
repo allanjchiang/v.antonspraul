63232495099548878a7f1c63215f4586b82d263a
Allan <allanchiang24@gmail.com>
2022-08-31 17:56:48 +1200

    Add formula for adding the two separate digits of a doubled number

--- functions/luhn-checksum.cpp
+++ functions/luhn-checksum.cpp
@@ -6,17 +6,19 @@ using std::cin;
 using std::cout; 
 
 
-int luhn_checksum = 123;
+int luhn_checksum = 156;
 
 int main() 
 {
 	int luhn_checksum_third = luhn_checksum % 10 * 2;
 	cout << luhn_checksum_third << endl;
+	int luhn_checksum_third_two_digits = luhn_checksum_third % 10 + luhn_checksum_third / 10 % 10;
+	cout << luhn_checksum_third_two_digits << endl;
 	int luhn_checksum_second = luhn_checksum / 10 % 10;
 	cout << luhn_checksum_second << endl;
 	int luhn_checksum_first = luhn_checksum / 100 % 10 * 2;
 	cout << luhn_checksum_first << endl;
-	if (((luhn_checksum_third + luhn_checksum_second + luhn_checksum_first) % 10) == 0) {
+	if (((luhn_checksum_third_two_digits + luhn_checksum_second + luhn_checksum_first) % 10) == 0) {
 		cout << "Valid!" << endl;
 	} else {
 		cout << "Invalid!" << endl;
