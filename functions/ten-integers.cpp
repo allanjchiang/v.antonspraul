08de2f933e5de447aefdd94b4899f576eb910646
Allan <allanchiang24@gmail.com>
2022-09-02 15:18:44 +1200

    Add function with for loops to count number of + and - numbers

--- functions/ten-integers.cpp
+++ functions/ten-integers.cpp
@@ -7,7 +7,34 @@ int integer;
 
 int main() 
 {
-	for(int i = 1; i <= 10; i++) {
-		cin >> integer;
+	int input[100], count, i, nCount = 0, pCount = 0, zCount = 0;
+
+	cout << "Enter Number of elements in array\n";
+	cin >> count;
+
+	cout << "Enter" << count << " numbers \n";
+
+	// Read elements
+	for (i = 0; i < count; i++) {
+		cin >> input[i];
 	}
+
+	// Iterate form index 0 to elementCount -1 and
+	 // check for positive negative and zero
+	for (i = 0; i < count; i++) {
+		if(input[i] < 0) {
+			nCount++;
+		} else if (input[i] > 0) {
+			pCount++;
+		}
+		else {
+			zCount++;
+		}
+	}
+
+	cout << "Negative Numbers: " << nCount << endl;
+	cout << "Positive Numbers: " << pCount << endl;
+	cout << "Zeroes: " << zCount << endl;
+
+	return 0;
 }
