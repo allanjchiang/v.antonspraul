43c3473643e6a052849a228d98b6355c856c28a3
Allan <allanchiang24@gmail.com>
2023-01-24 16:38:21 +1300

    Add nested if statement for calculating 4 and 5 digit numbers

---
+++ decode-a-message/intermediate-programs/three-four-or-five-digits.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+	cout << "Enter a number with 3, 4 or 5 digits: ";
+	char digitChar = cin.get();
+	int number = (digit - '0') * 100;
+	digitChar = cin.get();
+	number += (digit - '0') * 10;
+	digitChar = cin.get();
+	number += (digit - '0');
+	digitChar = cin.get();
+	if (digitChar == 10) {
+		cout << "Your 3 digit number is: " << number << "\n";
+	} else {
+		number = number * 10 + (digitChar - '0');
+		digitChar = cin.get();
+		if (digitChar == 10) {
+			cout << "Your 4 digit number is: " << number << "\n";
+		} else {
+			number = number * 10 + (digitChar - '0');
+			cout << "Your 5 digit number is: " << number << "\n";
+		}
+	}
+
+}
