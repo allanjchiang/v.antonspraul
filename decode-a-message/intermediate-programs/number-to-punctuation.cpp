5034f0376d6e690f88b2b77447f9d64e362b14eb
Allan <allanchiang24@gmail.com>
2023-01-25 15:45:13 +1300

    Add switch statement that converts numbers to punctuation marks

--- decode-a-message/intermediate-programs/number-to-punctuation.cpp
+++ decode-a-message/intermediate-programs/number-to-punctuation.cpp
@@ -8,4 +8,15 @@ int main()
 	int number;
 	cin >> number;
 	char outputCharacter;
+	switch (number) {
+	case 1: outputCharacter = '!';break;
+	case 2: outputCharacter = '?';break;
+	case 3: outputCharacter = ',';break;
+	case 4: outputCharacter = '.';break;
+	case 5: outputCharacter = ' ';break;
+	case 6: outputCharacter = ';';break;
+	case 7: outputCharacter = '"';break;
+	case 8: outputCharacter = '\'';break;
+	}
+	cout << "Equivalent symbol: " << outputCharacter << "\n";
 }
