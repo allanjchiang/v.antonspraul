4751907448cd741168d28c194814c93b92c8cfa2
Allan <allanchiang24@gmail.com>
2023-01-25 16:50:01 +1300

    Indent case statements to make code prettier

--- decode-a-message/decode-a-message-solution.cpp
+++ decode-a-message/decode-a-message-solution.cpp
@@ -37,13 +37,13 @@ int main()
 			number = number % 9;
 			switch (number) {
 				case 1: outputCharacter = '!';break;
-	case 2: outputCharacter = '?';break;
-	case 3: outputCharacter = ',';break;
-	case 4: outputCharacter = '.';break;
-	case 5: outputCharacter = ' ';break;
-	case 6: outputCharacter = ';';break;
-	case 7: outputCharacter = '"';break;
-	case 8: outputCharacter = '\'';break;
+				case 2: outputCharacter = '?';break;
+				case 3: outputCharacter = ',';break;
+				case 4: outputCharacter = '.';break;
+				case 5: outputCharacter = ' ';break;
+				case 6: outputCharacter = ';';break;
+				case 7: outputCharacter = '"';break;
+				case 8: outputCharacter = '\'';break;
 			}
 
 			if (number == 0) {
