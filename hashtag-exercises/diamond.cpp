9cd7a7d7991a3ab1f3102efe7e7e1a932246b27d
Allan <allanchiang24@gmail.com>
2023-02-22 16:35:04 +1300

    Change for loop variable to 5 - row

--- hashtag-exercises/diamond.cpp
+++ hashtag-exercises/diamond.cpp
@@ -6,7 +6,7 @@ using std::cout;
 int main()
 {
 	for(int row = 1; row < 5; row++){
-		for (int space = 1; space < 4 - row; space++){
+		for (int space = 1; space < 5 - row; space++){
 			cout << " ";
 		}
 		for (int hashNum = 1; hashNum < row + 1; hashNum++) {
