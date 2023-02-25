4725ea85898938be5e5d6103bc01cecd9c2300c9
Allan <allanchiang24@gmail.com>
2023-02-25 16:03:13 +1300

    Add for loop for hashtags of one side of logo

--- hashtag-exercises/hsbc.cpp
+++ hashtag-exercises/hsbc.cpp
@@ -2,3 +2,12 @@
 
 using std::cout;
 using std::cin;
+
+int main()
+{
+	for(int row = 1; row < 6; row++) {
+		for (int hashNum = 0; hashNum < 3 - abs(3 - row); hashNum++) {
+			cout << "#";
+		}
+	}
+}
