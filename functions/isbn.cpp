058f546a9e82832ec8aa4957751ef4a9304b399a
Allan <allanchiang24@gmail.com>
2023-02-26 16:06:16 +1300

    Add function for summing up all numbers in intNum

--- functions/isbn.cpp
+++ functions/isbn.cpp
@@ -3,7 +3,15 @@
 using std::cin;
 using std::cout;
 
+//idNumber = 16
+
+void sumDigits(int idNumber){
+	return idNumber % 10 + idNumber / 10 % 10;
+}
+
 int main()
 {
-	int idNumber = 123456789012;
+	
+	sumDigits(16);
+
 }
