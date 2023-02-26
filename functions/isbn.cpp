c991ade7ba305c0c8666df44618d19a6ce53c7bc
Allan <allanchiang24@gmail.com>
2023-02-26 16:44:19 +1300

    Add all digits to cout

--- functions/isbn.cpp
+++ functions/isbn.cpp
@@ -12,7 +12,7 @@ void sumDigits(long int idNumber){
 	int thirdDigit = idNumber / 1000000000 % 10;
 	int sumOfIdNumber = firstDigit + secondDigit + thirdDigit;
 
-	cout << firstDigit << " + " << secondDigit << " + " << thirdDigit << " = " << sumOfIdNumber;
+	cout << firstDigit << " + " << secondDigit << " + " << thirdDigit << " + " << fourthDigit << " + " << fifthDigit << " + " << sixthDigit << " + " << seventhDigit << " + " << eighthDigit << " + " << ninthDigit << " + " << tenthDigit << " + " << eleventhDigit << " + " << twelfthDigit << " = " << sumOfIdNumber;
 }
 
 int main()
