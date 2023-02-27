8ab1127640e224ae0dc230bf81329d8b6a9ccca2
Allan <allanchiang24@gmail.com>
2023-02-27 16:53:58 +1300

    Add 0 to each division to account for addition of 13th digit

--- functions/isbn.cpp
+++ functions/isbn.cpp
@@ -4,26 +4,26 @@ using std::cin;
 using std::cout;
 using namespace std;
 
-//idNumber = 123456789012
+//idNumber = 1234567890123
 
 void sumDigits(long int idNumber){
-	int firstDigit = idNumber / 100000000000 % 10;
-	int secondDigit = idNumber / 10000000000 % 10;
-	int secondDigitTripled = idNumber / 10000000000 % 10 * 3;
-	int thirdDigit = idNumber / 1000000000 % 10;
-	int fourthDigit = idNumber / 100000000 % 10;
-	int fourthDigitTripled = idNumber / 100000000 % 10 * 3;
-	int fifthDigit = idNumber / 10000000 % 10;
-	int sixthDigit = idNumber / 1000000 % 10;
-	int sixthDigitTripled = idNumber / 1000000 % 10 * 3;
-	int seventhDigit = idNumber / 100000 % 10;
-	int eighthDigit = idNumber / 10000 % 10;
-	int eighthDigitTripled = idNumber / 10000 % 10 * 3;
-	int ninthDigit = idNumber / 1000 % 10;
-	int tenthDigit = idNumber / 100 % 10;
-	int tenthDigitTripled = idNumber / 100 % 10 * 3;
-	int eleventhDigit = idNumber / 10 % 10;
-	int twelfthDigit = idNumber % 10;
+	int firstDigit = idNumber / 1000000000000 % 10;
+	int secondDigit = idNumber / 100000000000 % 10;
+	int secondDigitTripled = idNumber / 100000000000 % 10 * 3;
+	int thirdDigit = idNumber / 10000000000 % 10;
+	int fourthDigit = idNumber / 1000000000 % 10;
+	int fourthDigitTripled = idNumber / 1000000000 % 10 * 3;
+	int fifthDigit = idNumber / 100000000 % 10;
+	int sixthDigit = idNumber / 10000000 % 10;
+	int sixthDigitTripled = idNumber / 10000000 % 10 * 3;
+	int seventhDigit = idNumber / 1000000 % 10;
+	int eighthDigit = idNumber / 100000 % 10;
+	int eighthDigitTripled = idNumber / 100000 % 10 * 3;
+	int ninthDigit = idNumber / 10000 % 10;
+	int tenthDigit = idNumber / 1000 % 10;
+	int tenthDigitTripled = idNumber / 1000 % 10 * 3;
+	int eleventhDigit = idNumber / 100 % 10;
+	int twelfthDigit = idNumber / 10 % 10;
 	int twelfthDigitTripled = idNumber % 10 * 3;
 	int sumOfIdNumber = firstDigit + secondDigitTripled + thirdDigit + fourthDigitTripled + fifthDigit + sixthDigitTripled + seventhDigit + eighthDigitTripled + ninthDigit + tenthDigitTripled + eleventhDigit + twelfthDigitTripled;
 	int checkDigit = sumOfIdNumber % 10;
