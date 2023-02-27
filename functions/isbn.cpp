29c653fbcd9f8db65459271f0c8698e0842b7a8f
Allan <allanchiang24@gmail.com>
2023-02-27 16:10:06 +1300

    Triple each even digit in idNumber and sum all numbers together

--- functions/isbn.cpp
+++ functions/isbn.cpp
@@ -12,17 +12,23 @@ void sumDigits(long int idNumber){
 	int secondDigitTripled = idNumber / 10000000000 % 10 * 3;
 	int thirdDigit = idNumber / 1000000000 % 10;
 	int fourthDigit = idNumber / 100000000 % 10;
+	int fourthDigitTripled = idNumber / 100000000 % 10 * 3;
 	int fifthDigit = idNumber / 10000000 % 10;
 	int sixthDigit = idNumber / 1000000 % 10;
+	int sixthDigitTripled = idNumber / 1000000 % 10 * 3;
 	int seventhDigit = idNumber / 100000 % 10;
 	int eighthDigit = idNumber / 10000 % 10;
+	int eighthDigitTripled = idNumber / 10000 % 10 * 3;
 	int ninthDigit = idNumber / 1000 % 10;
 	int tenthDigit = idNumber / 100 % 10;
+	int tenthDigitTripled = idNumber / 100 % 10 * 3;
 	int eleventhDigit = idNumber / 10 % 10;
 	int twelfthDigit = idNumber % 10;
-	int sumOfIdNumber = firstDigit + secondDigitTripled + thirdDigit + fourthDigit + fifthDigit + sixthDigit + seventhDigit + eighthDigit + ninthDigit + tenthDigit + eleventhDigit + twelfthDigit;
+	int twelfthDigitTripled = idNumber % 10 * 3;
+	int sumOfIdNumber = firstDigit + secondDigitTripled + thirdDigit + fourthDigitTripled + fifthDigit + sixthDigitTripled + seventhDigit + eighthDigitTripled + ninthDigit + tenthDigitTripled + eleventhDigit + twelfthDigitTripled;
 
 	cout << firstDigit << "(1) + " << secondDigit << "(3) + " << thirdDigit << "(1) + " << fourthDigit << "(3) + " << fifthDigit << "(1) + " << sixthDigit << "(3) + " << seventhDigit << "(1) + " << eighthDigit << "(3) + " << ninthDigit << "(1) + " << tenthDigit << "(3) + " << eleventhDigit << "(1) + " << twelfthDigit << "(3)";
+	cout << "\n" << firstDigit << " + " << secondDigitTripled << " + " << thirdDigit << " + " << fourthDigitTripled << " + " << fifthDigit << " + " << sixthDigitTripled << " + " << seventhDigit << " + " << eighthDigitTripled << " + " << ninthDigit << " + " << tenthDigitTripled << " + " << eleventhDigit << " + " << twelfthDigitTripled << " = " << sumOfIdNumber;
 }
 
 int main()
